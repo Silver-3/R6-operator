ee29de332034a7ffb5cb569d501eb6f21d3a7a20
Silver <174689796+Silver-3@users.noreply.github.com>
2024-07-10 14:49:35 +1000

    Update

--- commands/random-operator.js
+++ commands/random-operator.js
@@ -51,42 +51,129 @@ module.exports = {
             operator = R6Info.getOperator(operator);
             const attachment = new Discord.AttachmentBuilder(operator.icon);
 
+            const row = new Discord.ActionRowBuilder()
+                .addComponents(
+                    new Discord.ButtonBuilder()
+                    .setCustomId('loadout')
+                    .setLabel('Randomize loadout?')
+                    .setStyle(Discord.ButtonStyle.Secondary)
+                )
+
             const embed = new Discord.EmbedBuilder()
                 .setTitle(`The random operator is: ${operator.name}`)
                 .setThumbnail(`attachment://${operator.name.toLowerCase()}.png`)
                 .setColor('Blurple')
-                .addFields(
-                    { name: 'Loadout', value: ' ', inline: true },{ name: ' ', value: ' ', inline: true },{ name: ' ', value: ' ', inline: true },
-                    { name: 'Primary Weapons', value: operator.guns.primary.join(' | '), inline: true },
-                    { name: 'Secondary Weapons', value: operator.guns.secondary.join(' | '), inline: true },
-                    { name: 'Gadgets', value: operator.gadgets.join(' | '), inline: true },
-                    { name: 'Information', value: ' ', inline: true },{ name: ' ', value: ' ', inline: true },{ name: ' ', value: ' ', inline: true },
-                    { name: `Ability: ${operator.ability.name}`, value: operator.ability.description, inline: true },
-                    { name: 'Specialties', value: operator.specialties.join(', '), inline: true },
-                    { name: 'Stats', value: `Health: ${operator.stats.health}\nSpeed: ${operator.stats.speed}\nDifficulty: ${operator.stats.difficulty}`, inline: true },
-                )
+                .addFields({
+                    name: 'Loadout',
+                    value: ' ',
+                    inline: true
+                }, {
+                    name: ' ',
+                    value: ' ',
+                    inline: true
+                }, {
+                    name: ' ',
+                    value: ' ',
+                    inline: true
+                }, {
+                    name: 'Primary Weapons',
+                    value: operator.guns.primary.join(' | '),
+                    inline: true
+                }, {
+                    name: 'Secondary Weapons',
+                    value: operator.guns.secondary.join(' | '),
+                    inline: true
+                }, {
+                    name: 'Gadgets',
+                    value: operator.gadgets.join(' | '),
+                    inline: true
+                }, {
+                    name: 'Information',
+                    value: ' ',
+                    inline: true
+                }, {
+                    name: ' ',
+                    value: ' ',
+                    inline: true
+                }, {
+                    name: ' ',
+                    value: ' ',
+                    inline: true
+                }, {
+                    name: `Ability: ${operator.ability.name}`,
+                    value: operator.ability.description,
+                    inline: true
+                }, {
+                    name: 'Specialties',
+                    value: operator.specialties.join(', '),
+                    inline: true
+                }, {
+                    name: 'Stats',
+                    value: `Health: ${operator.stats.health}\nSpeed: ${operator.stats.speed}\nDifficulty: ${operator.stats.difficulty}`,
+                    inline: true
+                }, )
 
             if (await db.has(interaction.user.id)) {
-                embed.setFooter({ text: 'Note: This operator has been added to used operators. Disable this with /remember disable' });
-                embed.addFields(
-                    { name: ' ', value: `\n\n*You can view your used operators with \`/used-operators\`*`,},
-                )
+                embed.setFooter({
+                    text: 'Note: This operator has been added to used operators. Disable this with /remember disable'
+                });
+                embed.addFields({
+                    name: ' ',
+                    value: `\n\n*You can view your used operators with \`/used-operators\`*`,
+                }, )
 
                 db.push(`${interaction.user.id}.operators.${team}`, operator.name.toLowerCase());
             }
 
-            interaction.reply({
+            const response = await interaction.reply({
                 embeds: [embed],
-                files: [attachment]
+                files: [attachment],
+                components: [row]
             });
-        }).catch(error => {
-            console.log(error.message);
 
+            const collector = await response.createMessageComponentCollector({
+                componentType: Discord.ComponentType.Button,
+                time: 10_000
+            });
+
+            collector.on('collect', async (i) => {
+                if (i.customId == 'loadout') {
+                    const primarys = operator.guns.primary;
+                    const secondarys = operator.guns.secondary;
+                    const gadgets = operator.gadgets;
+
+                    const loadout = {
+                        primary: primarys.length ? primarys[Math.floor(Math.random() * primarys.length)] : 'None',
+                        secondary: secondarys.length ? secondarys[Math.floor(Math.random() * secondarys.length)] : 'None',
+                        gadget: gadgets.length ? gadgets[Math.floor(Math.random() * gadgets.length)] : 'None'
+                    }
+
+                    embed.data.fields[0].name = 'Randomized loadout';
+                    embed.data.fields[3].name = 'Primary Weapon', embed.data.fields[3].value = loadout.primary;
+                    embed.data.fields[4].name = 'Secondary Weapon', embed.data.fields[4].value = loadout.secondary;
+                    embed.data.fields[5].name = 'Gadget', embed.data.fields[5].value = loadout.gadget;
+
+                    await i.update({
+                        embeds: [embed],
+                        components: []
+                    });
+                }
+            });
+
+            collector.on('end', () => {
+                interaction.editReply({
+                    embeds: [embed],
+                    components: []
+                });
+            })
+        }).catch(error => {
             const embed = new Discord.EmbedBuilder()
                 .setTitle('All operators have been used')
                 .setColor('Blurple')
                 .setDescription(`All operators on ${team} have been used. Use \`/reset\` to reset used operators.`)
-                .setFooter({ text: 'You can disable remembering used operators with /remember deactivate' });
+                .setFooter({
+                    text: 'You can disable remembering used operators with /remember deactivate'
+                });
 
             return interaction.reply({
                 embeds: [embed]
